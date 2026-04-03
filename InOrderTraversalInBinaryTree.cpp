a86ed9d996ab96a4967bbf77219e7b6a1b468919
React Native Developer <38905783+PreetiBaskey@users.noreply.github.com>
2026-04-03 08:19:37 +0530

    Update InOrderTraversalInBinaryTree.cpp

--- InOrderTraversalInBinaryTree.cpp
+++ InOrderTraversalInBinaryTree.cpp
@@ -1,3 +1,5 @@
+//Recursive approach
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -44,6 +46,77 @@ int main() {
     node* root = NULL;
     node* updatedRoot = createTree(root);
     cout<<"InOrder traversal is :- ";
+    inOrder(updatedRoot);
+    
+    return 0;
+}
+
+
+
+
+//Iterative approach
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        
+        Node(int data) {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* createTree(Node* root) {
+    int data;
+    cout<<"Enter data [Enter -1 to end] :- ";
+    cin>>data;
+    if(data == -1) {
+        return NULL;
+    }
+    root = new Node(data);
+    cout<<"Enter left data of "<<data<<endl;
+    root->left = createTree(root->left);
+    cout<<"Enter right data of "<<data<<endl;
+    root->right = createTree(root->right);
+    
+    return root;
+}
+
+void inOrder(Node* root) {
+    if(root == NULL) {
+        return;
+    }
+    
+    stack<Node*> st;
+    Node* curr = root;
+    
+    while(curr != NULL || !st.empty()) {
+        while(curr != NULL) {
+            st.push(curr);
+            curr = curr->left;
+        }
+        
+        curr = st.top();
+        st.pop();
+        
+        cout<<curr->data<<" ";
+        
+        curr = curr->right;
+    }
+}
+
+int main() {
+    
+    Node* root = NULL;
+    Node* updatedRoot = createTree(root);
+    cout<<"Inorder traversal is :- ";
     inOrder(updatedRoot);
     
     return 0;
